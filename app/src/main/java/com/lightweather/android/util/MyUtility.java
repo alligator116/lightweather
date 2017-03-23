d02bef0ab412a2fa977b381e126b615fd8dde329
wang <wanggang6012@sohu.com>
2017-03-24 00:39:23 +0800

    complete gothrough 3 level area list function

---
+++ app/src/main/java/com/lightweather/android/util/MyUtility.java
@@ -0,0 +1,92 @@
+package com.lightweather.android.util;
+
+import android.text.TextUtils;
+
+import com.lightweather.android.db.City;
+import com.lightweather.android.db.Country;
+import com.lightweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by gang on 2017/3/23.
+ */
+public class MyUtility {
+    public static boolean handleProvinceResponse(String response)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try
+            {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i=0; i<allProvinces.length();i++)
+                {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response, int provinceId)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try
+            {
+                JSONArray allCities = new JSONArray(response);
+                for(int i=0;i<allCities.length();i++)
+                {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountryResponse(String response, int cityId)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try{
+                JSONArray allCountries = new JSONArray(response);
+                for(int i=0;i<allCountries.length();i++)
+                {
+                    JSONObject countryObject = allCountries.getJSONObject(i);
+                    Country country = new Country();
+                    country.setCountryName(countryObject.getString("name"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+                return true;
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
